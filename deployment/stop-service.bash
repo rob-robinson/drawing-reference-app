5da1c1d0554ab8ca4b40ffb1d42d4c5602a6519c
Rob Robinson <robr@uoregon.edu>
2019-03-25 13:50:25 -0700

    use config.bash in stop-service

--- deployment/stop-service.bash
+++ deployment/stop-service.bash
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+source config.bash
+
 echo "=== stop and remove previous mysql container:"
-docker stop $(docker ps -aqf "name=image-search-db-container")
-docker rm $(docker ps -aqf "name=image-search-db-container")
+docker stop $(docker ps -aqf "name=${DB_CONTAINER}")
+docker rm $(docker ps -aqf "name=${DB_CONTAINER}")
 
 echo "=== stop and remove previous web app container:"
-docker stop $(docker ps -aqf "name=image-search-app-container")
-docker rm $(docker ps -aqf "name=image-search-app-container")
+docker stop $(docker ps -aqf "name=${APP_CONTAINER}")
+docker rm $(docker ps -aqf "name=${APP_CONTAINER}")
 
 echo "=== take down the network:"
-docker network rm image-search-app-bridge
+docker network rm ${NET_NAME}
