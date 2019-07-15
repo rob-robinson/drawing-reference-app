dfc72615c4934c9e0253676ec11718d8be3803ca
Rob Robinson <robr@uoregon.edu>
2019-07-15 10:18:40 -0700

    updating to docker compose
    removing manual orchestration

--- deployment/start-service.bash
+++ deployment/start-service.bash
@@ -1,45 +1,7 @@
 #!/usr/bin/env bash
 
-source config.bash
+#docker-compose build --no-cache
+docker-compose build
+#docker-compose up -d --force-recreate
+docker-compose up -d
 
-echo "=== Create The Image Search Service ===:"
-
-echo "=== stop and remove previous mysql container:"
-docker stop $(docker ps -aqf "name=${DB_CONTAINER}")
-docker rm $(docker ps -aqf "name=${DB_CONTAINER}")
-
-echo "=== stop and remove previous web app container:"
-docker stop $(docker ps -aqf "name=${APP_CONTAINER}")
-docker rm $(docker ps -aqf "name=${APP_CONTAINER}")
-
-echo "=== set up network:"
-docker network rm ${NET_NAME}
-docker network create ${NET_NAME}
-
-echo "=== build mysql:"
-docker build \
-    -t ${DB_IMAGE} \
-    ./db/
-
-echo "=== run the mysql container: ( this can take a long time if you have a bigger database 1G/Min or so... )"
-docker run \
-    --name ${DB_CONTAINER} \
-    -p 3306:3306 \
-    --network ${NET_NAME} \
-    -d \
-    ${DB_IMAGE}
-
-sleep 10
-
-echo "=== build app:"
-docker build \
-    -t ${APP_IMAGE} \
-    ./app/
-
-echo "===== run the app container:"
-docker run \
-    --name ${APP_CONTAINER} \
-    -p ${SERVICE_PORT}:5000 \
-    --network ${NET_NAME} \
-    -d \
-    ${APP_IMAGE}
--- deployment/stop-service.bash
+++ deployment/stop-service.bash
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 
-source config.bash
-
-echo "=== stop and remove previous mysql container:"
-docker stop $(docker ps -aqf "name=${DB_CONTAINER}")
-docker rm $(docker ps -aqf "name=${DB_CONTAINER}")
-
-echo "=== stop and remove previous web app container:"
-docker stop $(docker ps -aqf "name=${APP_CONTAINER}")
-docker rm $(docker ps -aqf "name=${APP_CONTAINER}")
-
-echo "=== take down the network:"
-docker network rm ${NET_NAME}
+docker-compose down
